04e580a5e86cab77de8293fd89f0b2d3d774ad13
marleigh <volcana@gmail.com>
2021-07-31 12:20:32 -0700

    cleaning out all the module stuff

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 //import { Archer, Warrior, Monk, Wizard } from './modules/classes.js'
-
 let id = 0;
 
 class Character {
@@ -25,8 +24,7 @@ class Character {
     }
 
 }
-
-class Wizard extends Character {
+ class Wizard extends Character {
     constructor(name, level) {
         super(name + "🧙🏽‍♀️", level)
         this.mana = 4 * level
@@ -56,7 +54,6 @@ class Wizard extends Character {
     }
 }
 
-
  class Warrior extends Character {
     constructor(name, level) {
         super(name + " ⚔️", level);
@@ -85,6 +82,51 @@ class Wizard extends Character {
 
     view() {
         return super.view( " Ki Level: " + this.ki);
+    }
+}
+
+/* 
+Section Review Challenge:
+
+    Create a new module, Enemies.js
+        It should include 3 new classes extending Character
+        Each should have their own placeholder ability
+            which we'll flesh out later:
+                Spider🕷 - bite()
+                Scorpion🦂 - sting()
+                Dragon🐉 - fireBreath()
+        The constructors for these classes should only require the enemy's level
+            The name will always just be the species 
+            + the emoji representation: `Dragon🐉`
+    
+    Then, update the Character class
+        -Add 2 new properties, hitpoints & maxHitpoints
+            both can be initialized to 4 times the Character's level
+        -Update the view method,
+            Finally, add a new div, that displays the Character's Hitpoints 
+            out of the maxHitpoints, this should have an id such as 'character-0-hitpoints' 
+            
+    Finally, create a new module called 'combat.js'
+        At this point, it should only export 1 function
+          startBattle(player, opponent)
+            This function should use Template Literals to 
+            construct and return a string with the views of both 
+            characters that are passed into it, 
+            and an additional button between 
+            them labelled "Attack" with an id of "attack-button"
+*/
+
+class Spider extends Character {
+    constructor(name, level) {
+        super(name + " 🕷 ", level)
+    }
+
+    bite() {
+        console.log("spider bite");
+    }
+
+    view() {
+        return super.view(" ")
     }
 }
 
